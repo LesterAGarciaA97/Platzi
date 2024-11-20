4863d569c66bd1dd087e19c79e3949ca2a762d10
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2024-11-20 12:15:53 -0600

    Adding laboratory part 01

---
+++ 03. Curso básico de testing en Java/Content/javatests/src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -0,0 +1,23 @@
+package com.platzi.javatests.util;
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+        String result = StringUtil.repeat("hola", 3);
+        //System.out.println(result);
+//        if (result.equals("holaholahola")) {
+//            System.out.println("OK");
+//        }
+        if (!result.equals("holaholahola")) {
+            System.out.println("ERROR");
+        }
+
+        String result2 = StringUtil.repeat("hola", 1);
+        //System.out.println(result2);
+//        if (result2.equals("hola")) {
+//            System.out.println("OK");
+//        }
+        if (!result2.equals("hola")) {
+            System.out.println("ERROR");
+        }
+    }
+}
