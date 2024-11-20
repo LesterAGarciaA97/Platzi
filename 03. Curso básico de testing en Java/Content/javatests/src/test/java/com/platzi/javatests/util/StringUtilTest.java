3683dd823b9f558c440b96ca1cc581faa653da9e
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2024-11-20 12:57:21 -0600

    Adding laboratory part 03

--- 03. Curso básico de testing en Java/Content/javatests/src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ 03. Curso básico de testing en Java/Content/javatests/src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -1,7 +1,10 @@
 package com.platzi.javatests.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         String result = StringUtil.repeat("hola", 3);
         //System.out.println(result);
 //        if (result.equals("holaholahola")) {
@@ -13,9 +16,14 @@ public class StringUtilTest {
 //        if (!result.equals("holaholahola")) {
 //            throw new RuntimeException("ERROR");
 //        }
-        assertEquals(result, "holaholahola");
+        //assertEquals(result, "holaholahola");
+        @Test
+        public void testRepeat(){
+            Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+            Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+        }
 
-        String result2 = StringUtil.repeat("hola", 1);
+        //String result2 = StringUtil.repeat("hola", 1);
         //System.out.println(result2);
 //        if (result2.equals("hola")) {
 //            System.out.println("OK");
@@ -26,12 +34,11 @@ public class StringUtilTest {
 //        if (!result2.equals("holahola")) {
 //            throw new RuntimeException("ERROR");
 //        }
-        assertEquals(result2, "hola");
+        //assertEquals(result2, "hola");
     }
-
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equal to " + expected);
-        }
-    }
-}
+//
+//    private static void assertEquals(String actual, String expected) {
+//        if (!actual.equals(expected)) {
+//            throw new RuntimeException(actual + " is not equal to " + expected);
+//        }
+//    }
