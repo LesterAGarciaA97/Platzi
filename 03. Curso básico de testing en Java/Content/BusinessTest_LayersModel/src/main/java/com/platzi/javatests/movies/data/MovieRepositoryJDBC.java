1fad2617b95e779503e8a401fc1437704fe9b575
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2024-11-21 15:36:36 -0600

    Adding Movies laboratory part 03

--- 03. Curso básico de testing en Java/Content/BusinessTest_LayersModel/src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJDBC.java
+++ 03. Curso básico de testing en Java/Content/BusinessTest_LayersModel/src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJDBC.java
@@ -20,7 +20,8 @@ public class MovieRepositoryJDBC implements MovieRepository {
 
     @Override
     public Movie findById(long id) {
-        return null;
+        Object[] args = { id };
+        return jdbcTemplate.queryForObject("select * from movies where id = ?", args, movieMapper);
     }
 
     @Override
@@ -30,6 +31,8 @@ public class MovieRepositoryJDBC implements MovieRepository {
 
     @Override
     public void saveOrUpdate(Movie movie) {
+        jdbcTemplate.update("insert into movies (name, minutes, genre) values (?, ?, ?)",
+                movie.getName(), movie.getMinutes(), movie.getGenre().toString());
     }
 
     //Original code before implementing Lambda
@@ -42,8 +45,15 @@ public class MovieRepositoryJDBC implements MovieRepository {
 //                    Genre.valueOf(rs.getString("genre")));
 //        }
 //    };
-    private static RowMapper<Movie> movieMapper = (rs, rowNum) -> new Movie(rs.getInt("id"),
-            rs.getString("name"),
-            rs.getInt("minutes"),
-            Genre.valueOf(rs.getString("genre")));
+//    private static RowMapper<Movie> movieMapper = (rs, rowNum) -> new Movie(rs.getInt("id"),
+//            rs.getString("name"),
+//            rs.getInt("minutes"),
+//            Genre.valueOf(rs.getString("genre")));
+//}
+    private static RowMapper<Movie> movieMapper = (rs, rowNum) ->
+            new Movie(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getInt("minutes"),
+                    Genre.valueOf(rs.getString("genre")));
 }
