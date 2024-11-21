0f41457ecf038cfeee10189475db803491915425
Lester Andres Garcia Aquino <lagarcia2015url@gmail.com>
2024-11-21 15:12:51 -0600

    Adding Movies laboratory part 02

---
+++ 03. Curso básico de testing en Java/Content/BusinessTest_LayersModel/src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJDBC.java
@@ -0,0 +1,49 @@
+package com.platzi.javatests.movies.data;
+
+import com.platzi.javatests.movies.model.Genre;
+import com.platzi.javatests.movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+public class MovieRepositoryJDBC implements MovieRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MovieRepositoryJDBC(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+        return jdbcTemplate.query("select * from movies", movieMapper);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+    }
+
+    //Original code before implementing Lambda
+//    private static RowMapper<Movie> movieMapper = new RowMapper<Movie>() {
+//        @Override
+//        public Movie mapRow(ResultSet rs, int rowNum) throws SQLException {
+//            return new Movie(rs.getInt("id"),
+//                    rs.getString("name"),
+//                    rs.getInt("minutes"),
+//                    Genre.valueOf(rs.getString("genre")));
+//        }
+//    };
+    private static RowMapper<Movie> movieMapper = (rs, rowNum) -> new Movie(rs.getInt("id"),
+            rs.getString("name"),
+            rs.getInt("minutes"),
+            Genre.valueOf(rs.getString("genre")));
+}
